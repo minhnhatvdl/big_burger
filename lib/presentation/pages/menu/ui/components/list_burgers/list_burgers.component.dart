3fe23e8b4eb26ae560442a4650275a92bd547813
Minh <minh.nhat@helios.do>
2022-12-10 19:21:43 +0100

    Add a button of refresh menu page when having an server error

--- lib/presentation/pages/menu/ui/components/list_burgers/list_burgers.component.dart
+++ lib/presentation/pages/menu/ui/components/list_burgers/list_burgers.component.dart
@@ -11,8 +11,11 @@ class ListBurgers extends StatelessWidget {
   const ListBurgers({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final menuBloc = context.read<MenuBloc>();
+
     return StateNotifierBuilder<MenuState>(
-      stateNotifier: context.read<MenuBloc>(),
+      stateNotifier: menuBloc,
       builder: (_, MenuState state, __) {
         if (state is LoadingMenuState) {
           return SliverList(
@@ -27,6 +30,32 @@ class ListBurgers extends StatelessWidget {
             delegate: SliverChildBuilderDelegate(
               (_, int index) => BurgerItem(menu[index]),
               childCount: menu.length,
+            ),
+          );
+        } else if (state is ErrorMenuState) {
+          return SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    'Malheureusement, une erreur du serveur nous a empêché de finaliser votre demande. Merci de réessayer.',
+                    style: theme.textTheme.subtitle2,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(shape: const StadiumBorder(), backgroundColor: theme.primaryColor),
+                  onPressed: menuBloc.getMenu,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      'Rafraîchir',
+                      style: theme.textTheme.subtitle1?.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
             ),
           );
         }
